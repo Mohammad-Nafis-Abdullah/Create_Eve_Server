e6cadfe303c91b213461d047f6b9c2e0a1e2808a
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-05-14 01:29:01 +0600

    cors config changed

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const serviceUpload = multer({
   },
 });
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
